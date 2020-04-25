85b39d12f760fea73a5c0b4b90f1e6135ad661dc
Youssef alaa <yalaa129@gmail.com>
2020-04-25 04:40:47 +0300

    updating system

--- hangman.py
+++ hangman.py
@@ -8,7 +8,7 @@ whileHolder = True
 def end_msg(randword, is_success):
     if is_success:
         print('')
-        print(randword + '\nYou guessed the word!\nYou survived!')
+        print(randword + '\nYou guessed the word!\nYou survived!\n')
     else:
         print('You are hanged!')
         print('')
@@ -26,11 +26,10 @@ def blanks_parser(randword):
     return blanks
 
 
-def hangman():
-    randword = random.choice(words.physics)
+def hangman(wordlist):
+    randword = random.choice(wordlist)
     tries = set()
     chances = 8
-    not_ascii = '~!@#$%^&*()_+`1234567890-=[]\\{}|;\':",./<>?'
     blanks = blanks_parser(randword)
 
     while chances > 0:
@@ -54,7 +53,7 @@ def hangman():
                 break
         elif guess in tries:
             print('You already typed this letter')
-        elif guess in not_ascii:
+        elif not guess.isalpha():
             print('This is not an English letter')
         else:
             tries.add(guess)
@@ -69,11 +68,19 @@ def hangman():
 
 print('H A N G M A N')
 while whileHolder:
-    menu = input('Type "play" to play the game, "exit" to quit: ')
-    if menu == 'play':
-        hangman()
-    elif menu == 'exit':
+    menu = input('Select an option from the list:\n1: play\n2: exit\n> ')
+    if menu == '1':
+        word_menu = input('Select a category from the list:\n1: chemistry\n2: physics\n3: biology\n> ')
+        if word_menu == '1':
+            print('type /help for help or /whiteflag if you surrender\n')
+            hangman(words.chemistry)
+        elif word_menu == '2':
+            print('type /help for help or /whiteflag if you surrender\n')
+            hangman(words.physics)
+        else:
+            print('type /help for help or /whiteflag if you surrender\n')
+            hangman(words.biology)
+    elif menu == '2':
         quit()
     else:
         continue
-
--- words.py
+++ words.py
@@ -8,5 +8,30 @@ physics = [
     'static friction',
     'kinetic friction',
     'friction',
-    'energy'
+    'energy',
+    'atom',
+    'electron',
+    'proton'
+]
+biology = [
+    'cell',
+    'mitochondria',
+    'heredity',
+    'reproduction',
+    'sperm',
+    'egg',
+    'mammals',
+    'amphibians',
+    'reptiles',
+    'birds'
+]
+chemistry = [
+    'atom',
+    'electron',
+    'proton',
+    'neutron',
+    'oxidation',
+    'periodic table',
+    'carbon',
+    'hydrogen'
 ]
